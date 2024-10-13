a11ea6983e194c93d0d6f410d1f524f41e1552f8
Oliver Rainoch <540484@mail.muni.cz>
2024-10-13 21:37:30 +0200

    fix: removed unused function from main

--- mammaprint/__main__.py
+++ mammaprint/__main__.py
@@ -20,8 +20,6 @@ OmegaConf.register_new_resolver(
 def main(config: DictConfig, logger: Logger | None) -> None:
     seed_everything(config.seed, workers=True)
 
-    torch.set_float32_matmul_precision(precision="high")
-
     data = hydra.utils.instantiate(
         config.data,
         _recursive_=False,  # to avoid instantiating all the datasets
