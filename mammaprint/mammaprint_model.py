539096c281ea6811b8387450696858c7d259793f
Oliver Rainoch <540484@mail.muni.cz>
2024-10-14 17:13:31 +0200

    feat: update train metrics every step

--- mammaprint/mammaprint_model.py
+++ mammaprint/mammaprint_model.py
@@ -25,6 +25,7 @@ class MammaprintModel(LightningModule):
             }
         )
         self.test_metrics = LazyMetricDict(self.val_metrics.clone())
+        self.train_metrics = LazyMetricDict(self.val_metrics.clone())
         self.val_metrics.prefix = "validation/"
 
     def forward(self, x: Tensor) -> Outputs:
@@ -39,7 +40,8 @@ class MammaprintModel(LightningModule):
         self.log(
             "train/loss", loss, batch_size=len(inputs), on_step=True, prog_bar=True
         )
-
+        self.train_metrics.update(outputs, targets)
+        self.log_dict(self.train_metrics, batch_size=len(inputs), on_step=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch: Input) -> None:
