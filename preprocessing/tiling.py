b29e5eed57b8bb2f821dd46b877b481d523cdc1d
Oliver Rainoch <540484@mail.muni.cz>
2024-10-16 10:03:35 +0200

    exp: tiling at level 0

--- preprocessing/tiling.py
+++ preprocessing/tiling.py
@@ -11,8 +11,8 @@ from rationai.tiling.writers import save_mlflow_dataset
 from sklearn.model_selection import train_test_split
 
 
-SLIDES_PATH = "/mnt/data/Projects/MOU/Mammaprint/Test_set_mamaprint_tiff/"
-TISSUE_MASKS_PATH = "/mnt/data/Projects/MOU/Mammaprint/Test_set_mamaprint_tissue_masks/"
+SLIDES_PATH = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs/"
+TISSUE_MASKS_PATH = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs_tissue_masks/"
 ANNOTATION_MASKS_PATH = ""
 
 
@@ -39,7 +39,7 @@ class TissueMask(PyvipsMask[TileMetadata]):
 #         )
 
 
-source = OpenSlideTileSource(mpp=0.48, tile_extent=512, stride=256)
+source = OpenSlideTileSource(mpp=0.25, tile_extent=512, stride=256)
 tissue_mask = TissueMask(
     tile_extent=source.tile_extent, absolute_roi_extent=256, relative_roi_offset=0
 )
