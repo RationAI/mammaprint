79f7c9e0eef993f08003ba6fef279f7b9946c5ba
Oliver Rainoch <540484@mail.muni.cz>
2024-10-17 01:19:22 +0200

    fix: missing callbacks

--- mammaprint/trainer/callbacks/__init__.py
+++ mammaprint/trainer/callbacks/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) The RationAI team.
 
-from mammaprint.trainer.callbacks.cropped_heatmaps import CroppedHeatmaps
 from mammaprint.trainer.callbacks.dataloader_agnostic import (
     DataloaderAgnosticCallback,
 )
@@ -8,9 +7,6 @@ from mammaprint.trainer.callbacks.heatmap_visualizer import HeatmapVisualizer
 from mammaprint.trainer.callbacks.image_builders import (
     DiskMappedPatchAssembler,
     ImageBuilder,
-)
-from mammaprint.trainer.callbacks.mlflow_model_checkpoint import (
-    MLFlowModelCheckpoint,
 )
 from mammaprint.trainer.callbacks.slide_aggregation import (
     SlidePredictionOptimizer,
@@ -23,8 +19,6 @@ __all__ = [
     "HeatmapVisualizer",
     "ImageBuilder",
     "DiskMappedPatchAssembler",
-    "MLFlowModelCheckpoint",
-    "CroppedHeatmaps",
     "SlidePredictor",
     "SlidePredictionOptimizer",
 ]
