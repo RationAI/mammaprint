e3c00ad37a523cdcdf9818b3aecc7a8195ebc2dd
Oliver Rainoch <540484@mail.muni.cz>
2024-10-24 17:34:34 +0200

    exp: changed classifier, dimension to 256, lr to 1e-4

--- mammaprint/ml/nets/attmil.py
+++ mammaprint/ml/nets/attmil.py
@@ -7,7 +7,7 @@ import torchvision.models as models
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class GatedAttention(nn.Module):
-    def __init__(self, feature_dim, attention_dim=512):
+    def __init__(self, feature_dim, attention_dim=256):
         super(GatedAttention, self).__init__()
         self.attention_V = nn.Linear(feature_dim, attention_dim)
         self.attention_U = nn.Linear(feature_dim, attention_dim)
@@ -26,15 +26,15 @@ class AttMILModel(nn.Module):
     def __init__(self):
         super(AttMILModel, self).__init__()
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.attention = GatedAttention(512)
+        self.attention = GatedAttention(256)
         self.classifier = nn.Sequential(
+            nn.Linear(512, 512),  # Increased to 512 units
+            nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(128, 1)
+            nn.Dropout(0.5),
+            nn.Linear(256, 1)
         )
 
     def forward(self, x):
