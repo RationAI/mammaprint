f108e3fd7afc7165d3fa8245357cdeab3c6b765a
Oliver Rainoch <540484@mail.muni.cz>
2024-10-24 14:12:39 +0200

    fix: importing attmilmodel

--- mammaprint/ml/nets/__init__.py
+++ mammaprint/ml/nets/__init__.py
@@ -3,7 +3,7 @@
 from mammaprint.ml.nets.resnet import ResNet50, ResNet101, ResNet152
 from mammaprint.ml.nets.saved_model import SavedModel
 from mammaprint.ml.nets.vgg16 import BinaryClassifier, GMaxPool, VGG16Features
-
+from mammaprint.ml.nets.attmil import AttMILModel
 
 __all__ = [
     "SavedModel",
@@ -13,4 +13,5 @@ __all__ = [
     "ResNet101",
     "ResNet152",
     "VGG16Features",
+    "AttMILModel",
 ]
