12dc312ac1a0cb6116cbd0757d6990e96b74232a
Oliver Rainoch <540484@mail.muni.cz>
2024-10-31 20:27:07 +0100

    exp: tiling test set after tissue classification model

--- preprocessing/tiling.py
+++ preprocessing/tiling.py
@@ -11,14 +11,13 @@ from rationai.tiling.writers import save_mlflow_dataset
 from sklearn.model_selection import train_test_split
 
 
-SLIDES_PATH = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs_tiff/"
-TISSUE_MASKS_PATH = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs_tissue_masks/"
-ANNOTATION_MASKS_PATH = ""
+SLIDES_PATH = "/mnt/data/Projects/MOU/Mammaprint/Test_set_mammaprint/"
+TISSUE_MASKS_PATH = "/mnt/data/Projects/MOU/Mammaprint/Test_set_mammaprint_tiff/"
+ANNOTATION_MASKS_PATH = "/mnt/data/Projects/MOU/Mammaprint/Test_set_tissue_classification_tumor_masks/test_heatmaps/"
 
-
-# @dataclass
-# class CancerTileMetadata(TileMetadata):
-#     cancer_percentage: float
+@dataclass
+class CancerTileMetadata(TileMetadata):
+    cancer_percentage: float
 
 
 class TissueMask(PyvipsMask[TileMetadata]):
@@ -30,22 +29,22 @@ class TissueMask(PyvipsMask[TileMetadata]):
         return tile_labels
 
 
-# class CancerMask(PyvipsMask[CancerTileMetadata]):
-#     def forward_tile(
-#         self, tile_labels: TileMetadata, class_overlaps: dict[int, float]
-#     ) -> CancerTileMetadata:
-#         return CancerTileMetadata(
-#             **asdict(tile_labels), cancer_percentage=class_overlaps.get(255, 0)
-#         )
+class CancerMask(PyvipsMask[CancerTileMetadata]):
+    def forward_tile(
+        self, tile_labels: TileMetadata, class_overlaps: dict[int, float]
+    ) -> CancerTileMetadata:
+        return CancerTileMetadata(
+            **asdict(tile_labels), cancer_percentage=class_overlaps.get(255, 0)
+        )
 
 
 source = OpenSlideTileSource(mpp=0.25, tile_extent=512, stride=256)
 tissue_mask = TissueMask(
     tile_extent=source.tile_extent, absolute_roi_extent=256, relative_roi_offset=0
 )
-# cancer_mask = CancerMask(
-#     tile_extent=source.tile_extent, absolute_roi_extent=256, relative_roi_offset=0
-# )
+cancer_mask = CancerMask(
+    tile_extent=source.tile_extent, absolute_roi_extent=256, relative_roi_offset=0
+)
 
 
 @ray.remote
@@ -53,10 +52,10 @@ def handler(slide_path: Path) -> TiledSlideMetadata:
     slide, tiles = source(slide_path)
 
     tissue_mask_path = Path(TISSUE_MASKS_PATH, slide_path.name)
-    #cancer_mask_path = Path(ANNOTATION_MASKS_PATH, slide_path.name)
+    cancer_mask_path = Path(ANNOTATION_MASKS_PATH, slide_path.name)
 
     tiles = tissue_mask(tissue_mask_path, slide.extent, tiles)
-    #tiles = cancer_mask(cancer_mask_path, slide.extent, tiles)
+    tiles = cancer_mask(cancer_mask_path, slide.extent, tiles)
 
     return slide, tiles
 
