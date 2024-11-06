0acdd9c9e9ed11770c056f7ae83d1020c95ee926
Oliver Rainoch <540484@mail.muni.cz>
2024-11-07 00:14:45 +0100

    fix: attention weights in module

--- mammaprint/ml/mammaprint_module.py
+++ mammaprint/ml/mammaprint_module.py
@@ -44,7 +44,7 @@ class mammaprintModule(lightning.pytorch.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_pred, attention_weights = self(x)
+        y_pred = self(x)
 
         # Update and log loss
         loss = self.loss(y_pred, y)
@@ -62,7 +62,7 @@ class mammaprintModule(lightning.pytorch.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_pred, attention_weights = self(x)
+        y_pred = self(x)
 
         # Update and log loss
         loss = self.loss(y_pred, y)
@@ -84,7 +84,7 @@ class mammaprintModule(lightning.pytorch.LightningModule):
     @torch.inference_mode()
     def test_step(self, batch, batch_idx, dataloader_idx=None):
         x, y, metadata = batch
-        y_pred, attention_weights = self(x)
+        y_pred = self(x)
 
         if "slide_name" in metadata[0]:
             slide_name = metadata[0]["slide_name"][0]
@@ -94,22 +94,22 @@ class mammaprintModule(lightning.pytorch.LightningModule):
         else:
             slide_name = dataloader_idx
 
-        # Process attention weights here
-        attention_weights_flat = attention_weights.view(-1).detach().cpu().numpy()
-        coord_x = np.concatenate([md['coord_x'] for md in metadata])
-        coord_y = np.concatenate([md['coord_y'] for md in metadata])
-        attention_data = {
-            'weights': attention_weights_flat,
-            'coord_x': coord_x,
-            'coord_y': coord_y
-        }
+        # # Process attention weights here
+        # attention_weights_flat = attention_weights.view(-1).detach().cpu().numpy()
+        # coord_x = np.concatenate([md['coord_x'] for md in metadata])
+        # coord_y = np.concatenate([md['coord_y'] for md in metadata])
+        # attention_data = {
+        #     'weights': attention_weights_flat,
+        #     'coord_x': coord_x,
+        #     'coord_y': coord_y
+        # }
         self.metrics.update("test", y_pred, y, slide_name)
         self.log_dict(self.metrics.get("test", slide_name), add_dataloader_idx=False)
 
         return {
             "metrics": self.metrics.compute("test", slide_name),
             "outputs": y_pred,
-            "attention_data": attention_data,
+            # "attention_data": attention_data,
             "metadata": metadata,
         }
 
