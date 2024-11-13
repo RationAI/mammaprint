52e84078f87aef36ef1e132a9788d9175ba0743b
Oliver Rainoch <540484@mail.muni.cz>
2024-11-13 15:35:36 +0100

    fix: original mammaprint module

--- mammaprint/ml/mammaprint_module.py
+++ mammaprint/ml/mammaprint_module.py
@@ -8,8 +8,6 @@ import lightning
 import torch
 import torchmetrics
 
-import numpy as np
-
 from mammaprint.ml.metrics import MetricDictionary
 
 
@@ -38,13 +36,13 @@ class mammaprintModule(lightning.pytorch.LightningModule):
         self.register_metric_dict("metrics", metrics)
 
     def forward(self, x):
-        x, attention_weights = self.model(x)
+        x = self.model(x)
         x = self.output_activation(x)
-        return x, attention_weights
+        return x
 
     def training_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_pred, attention_weights = self(x)
+        y_pred = self(x)
 
         # Update and log loss
         loss = self.loss(y_pred, y)
@@ -62,7 +60,7 @@ class mammaprintModule(lightning.pytorch.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_pred, attention_weights = self(x)
+        y_pred = self(x)
 
         # Update and log loss
         loss = self.loss(y_pred, y)
@@ -84,42 +82,27 @@ class mammaprintModule(lightning.pytorch.LightningModule):
     @torch.inference_mode()
     def test_step(self, batch, batch_idx, dataloader_idx=None):
         x, y, metadata = batch
-        y_pred, attention_weights = self(x) 
+        y_pred = self(x)
 
-        if "slide_name" in metadata[0]:
-            slide_name = metadata[0]["slide_name"][0]
+        # if "slide_name" in metadata[0]:
+        #     slide_name = metadata[0]["slide_name"][0]
         # # Update and log metrics
-        # if "slide_name" in metadata:
-        #     slide_name = metadata["slide_name"][0]
+        if "slide_name" in metadata:
+            slide_name = metadata["slide_name"][0]
         else:
             slide_name = dataloader_idx
-
-        # # Process attention weights here
-        # attention_weights_flat = attention_weights.view(-1).detach().cpu().numpy()
-        # coord_x = np.concatenate([md['coord_x'] for md in metadata])
-        # coord_y = np.concatenate([md['coord_y'] for md in metadata])
-        # attention_data = {
-        #     'weights': attention_weights_flat,
-        #     'coord_x': coord_x,
-        #     'coord_y': coord_y
-        # }
         self.metrics.update("test", y_pred, y, slide_name)
         self.log_dict(self.metrics.get("test", slide_name), add_dataloader_idx=False)
 
-        return {
-            "metrics": self.metrics.compute("test", slide_name),
-            "outputs": y_pred,
-            "attention_weights": attention_weights,
-            "metadata": metadata,
-        }
+        return {"metrics": self.metrics.compute("test", slide_name), "outputs": y_pred}
 
     def on_test_epoch_end(self):
         self.log_dict(self.metrics.get("test"))
 
     def predict_step(self, batch, batch_idx, dataloader_idx=None):
         x, y, _ = batch
-        y_pred, attention_weights = self(x)  # Extract both output and attention weights
-        return {"outputs": y_pred, "attention_weights": attention_weights}  # Return attention weights too
+        y_pred = self(x)
+        return {"outputs": y_pred}
 
     def configure_optimizers(self):
         if self.optimizer is None:
---
+++ mammaprint/ml/mammaprint_module_mil.py
@@ -0,0 +1,145 @@
+# # Copyright (c) The RationAI team.
+
+# import functools
+# import logging
+# from collections import OrderedDict
+
+# import lightning
+# import torch
+# import torchmetrics
+
+# import numpy as np
+
+# from mammaprint.ml.metrics import MetricDictionary
+
+
+# logger = logging.getLogger("mammaprint_module")
+
+
+# class mammaprintModule(lightning.pytorch.LightningModule):
+#     def __init__(
+#         self,
+#         net: torch.nn.Module | dict[str, torch.nn.Module],
+#         output_activation: torch.nn.Module | None = None,
+#         loss: torch.nn.Module | None = None,
+#         optimizer: functools.partial | None = None,
+#         lr_scheduler: torch.optim.lr_scheduler.LRScheduler | None = None,
+#         metrics: dict[str, torchmetrics.Metric] | None = None,
+#     ) -> None:
+#         super().__init__()
+#         if isinstance(net, dict):
+#             net = torch.nn.Sequential(OrderedDict(net))
+#         self.model = net
+#         self.loss = loss
+#         self.optimizer = optimizer
+#         self.lr_scheduler = lr_scheduler
+#         self.output_activation = output_activation
+
+#         self.register_metric_dict("metrics", metrics)
+
+#     def forward(self, x):
+#         x, attention_weights = self.model(x)
+#         x = self.output_activation(x)
+#         return x, attention_weights
+
+#     def training_step(self, batch, batch_idx):
+#         x, y, _ = batch
+#         y_pred, attention_weights = self(x)
+
+#         # Update and log loss
+#         loss = self.loss(y_pred, y)
+#         self.log("train/loss", loss, on_step=True, on_epoch=True)
+
+#         # Update and log metrics
+#         self.metrics.update("train", y_pred, y)
+#         self.log_dict(self.metrics.get("train"), on_step=True)
+
+#         return {
+#             "loss": loss,
+#             "metrics": self.metrics.compute("train"),
+#             "outputs": y_pred,
+#         }
+
+#     def validation_step(self, batch, batch_idx):
+#         x, y, _ = batch
+#         y_pred, attention_weights = self(x)
+
+#         # Update and log loss
+#         loss = self.loss(y_pred, y)
+#         self.log("valid/loss", loss)
+
+#         # Update and log metrics
+#         self.metrics.update("valid", y_pred, y)
+#         self.log_dict(self.metrics.get("valid"), add_dataloader_idx=False)
+
+#         return {
+#             "loss": loss,
+#             "metrics": self.metrics.compute("valid"),
+#             "outputs": y_pred,
+#         }
+
+#     def on_validation_epoch_end(self):
+#         self.log_dict(self.metrics.get("valid"))
+
+#     @torch.inference_mode()
+#     def test_step(self, batch, batch_idx, dataloader_idx=None):
+#         x, y, metadata = batch
+#         y_pred, attention_weights = self(x) 
+
+#         if "slide_name" in metadata[0]:
+#             slide_name = metadata[0]["slide_name"][0]
+#         # # Update and log metrics
+#         # if "slide_name" in metadata:
+#         #     slide_name = metadata["slide_name"][0]
+#         else:
+#             slide_name = dataloader_idx
+
+#         # # Process attention weights here
+#         # attention_weights_flat = attention_weights.view(-1).detach().cpu().numpy()
+#         # coord_x = np.concatenate([md['coord_x'] for md in metadata])
+#         # coord_y = np.concatenate([md['coord_y'] for md in metadata])
+#         # attention_data = {
+#         #     'weights': attention_weights_flat,
+#         #     'coord_x': coord_x,
+#         #     'coord_y': coord_y
+#         # }
+#         self.metrics.update("test", y_pred, y, slide_name)
+#         self.log_dict(self.metrics.get("test", slide_name), add_dataloader_idx=False)
+
+#         return {
+#             "metrics": self.metrics.compute("test", slide_name),
+#             "outputs": y_pred,
+#             "attention_weights": attention_weights,
+#             "metadata": metadata,
+#         }
+
+#     def on_test_epoch_end(self):
+#         self.log_dict(self.metrics.get("test"))
+
+#     def predict_step(self, batch, batch_idx, dataloader_idx=None):
+#         x, y, _ = batch
+#         y_pred, attention_weights = self(x)  # Extract both output and attention weights
+#         return {"outputs": y_pred, "attention_weights": attention_weights}  # Return attention weights too
+
+#     def configure_optimizers(self):
+#         if self.optimizer is None:
+#             return None
+
+#         optimizer = self.optimizer(self.model.parameters())
+#         ret = {"optimizer": optimizer}
+
+#         if self.lr_scheduler:
+#             ret["lr_scheduler"] = {
+#                 "scheduler": self.lr_scheduler["scheduler"](optimizer)
+#             }
+
+#             monitor = self.lr_scheduler.get("monitor")
+#             if monitor:
+#                 ret["lr_scheduler"]["monitor"] = monitor
+
+#         return ret
+
+#     def register_metric_dict(self, parameter_name: str, metrics: dict | None) -> None:
+#         metric_collection = torchmetrics.MetricCollection(metrics or {})
+#         metric_module = MetricDictionary(metric_collection, self, parameter_name)
+#         self.add_module(parameter_name, metric_module)
