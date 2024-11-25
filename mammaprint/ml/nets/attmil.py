8bee979830e4f58240956d19e36c114c17965a7b
Oliver Rainoch <540484@mail.muni.cz>
2024-11-25 19:07:16 +0100

    exp: training baseline luminal MIL model

--- mammaprint/ml/nets/attmil.py
+++ mammaprint/ml/nets/attmil.py
@@ -14,7 +14,7 @@ import torchvision.models as models
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
   
 class GatedAttention(nn.Module):
-    def __init__(self, feature_dim, attention_dim=512):
+    def __init__(self, feature_dim, attention_dim=256):
         super(GatedAttention, self).__init__()
         self.attention_V = nn.Linear(feature_dim, attention_dim)
         self.attention_U = nn.Linear(feature_dim, attention_dim)
@@ -39,7 +39,7 @@ class AttMILModel(nn.Module):
         self.classifier = nn.Sequential(
             nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.1),
             nn.Linear(256, 1)
         )
         
