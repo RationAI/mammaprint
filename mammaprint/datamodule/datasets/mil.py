fa1501a936915c5053dcbf73e4eae01ab8d8a637
Oliver Rainoch <540484@mail.muni.cz>
2024-11-29 19:23:37 +0100

    fix: label to luminal id

--- mammaprint/datamodule/datasets/mil.py
+++ mammaprint/datamodule/datasets/mil.py
@@ -6,7 +6,7 @@ from mammaprint.datamodule.samplers import BaseSampler
 import torch
 
 class MILDataset(BaseDataset):
-    def __init__(self, sampler: BaseSampler, seed, augmentations: albumentations.TemplateTransform | None = None, label: str = "class_id") -> None:
+    def __init__(self, sampler: BaseSampler, seed, augmentations: albumentations.TemplateTransform | None = None, label: str = "luminal_id") -> None:
         super().__init__(sampler=sampler, seed=seed)
         self.transforms = augmentations
         self.label = label
@@ -29,7 +29,10 @@ class MILDataset(BaseDataset):
         # label_value = next((tile.get(self.label) for tile in sample if tile.get(self.label, 0.0) != 0.0), 0.0)
         # # logging.info(f"label value found: {label_value}")
         # label = torch.tensor([label_value])
-        label = torch.tensor([float(sample[0]['is_cancer'])])  # Encapsulate the float in a list
+        label_value = next((tile.get(self.label) for tile in sample if tile.get(self.label, 0.0) != 0.0), 0.0)
+        # logging.info(f"label value found: {label_value}")
+        label = torch.tensor([label_value])
+
         logging.debug(f"Label: {label}")
 
 
