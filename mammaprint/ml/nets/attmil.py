74fccdf190d19b272af39ded7c227f669ee10d81
Oliver Rainoch <540484@mail.muni.cz>
2024-12-04 14:34:51 +0100

    exp: increased dropout to 0.5

--- mammaprint/ml/nets/attmil.py
+++ mammaprint/ml/nets/attmil.py
@@ -30,7 +30,7 @@ class GatedAttention(nn.Module):
         return A
 
 class AttMILModel(nn.Module):
-    def __init__(self, feature_dim=2048, classifier_dims= [512, 256, 1], dropout=0.1):
+    def __init__(self, feature_dim=2048, classifier_dims= [512, 256, 1], dropout=0.5):
         super(AttMILModel, self).__init__()
         self.logger = logging.getLogger(self.__class__.__name__)
         # Layer normalization for input stability
