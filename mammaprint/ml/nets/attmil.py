2a4f357caa8a1f41a773251e934a350d3e4a893a
Oliver Rainoch <540484@mail.muni.cz>
2024-12-05 20:23:16 +0100

    exp: using resnet50 features but same configuration and seed as the best model

--- mammaprint/ml/nets/attmil.py
+++ mammaprint/ml/nets/attmil.py
@@ -14,7 +14,7 @@ import torchvision.models as models
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
   
 class GatedAttention(nn.Module):
-    def __init__(self, feature_dim, attention_dim=512):
+    def __init__(self, feature_dim, attention_dim=2048):
         super(GatedAttention, self).__init__()
         self.attention_V = nn.Linear(feature_dim, attention_dim)
         self.attention_U = nn.Linear(feature_dim, attention_dim)
@@ -30,7 +30,7 @@ class GatedAttention(nn.Module):
         return A
 
 class AttMILModel(nn.Module):
-    def __init__(self, feature_dim=512, dropout=0.2):
+    def __init__(self, feature_dim=2048, dropout=0.2):
         super(AttMILModel, self).__init__()
         self.logger = logging.getLogger(self.__class__.__name__)
         # Layer normalization for input stability
