bf616bfc411f7f16886178a5f427348480cf721f
Matěj Pekár <525077@mail.muni.cz>
2025-03-08 18:43:19 +0100

    fix: tissue masks

--- preprocessing/generate_tissue_masks.py
+++ preprocessing/generate_tissue_masks.py
@@ -1,53 +1,42 @@
 from pathlib import Path
 from statistics import mean
+from typing import Any
 
-# from typing import Any
-import ray
 from openslide import OpenSlide
 from pyvips import Image
-from rationai.masks import process_items, slide_resolution, tissue_mask, write_big_tiff
+from rationai.masks import slide_resolution, tissue_mask, write_big_tiff
+from ray import data
 
-# from ray import data
-
-SLIDES_PATH = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs"
-MASK_DEST = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs_tissue_masks"
+SLIDES_PATHS = [
+    "/mnt/data/Projects/MOU/Mammaprint/Learnig_set_mamaprint",
+    "/mnt/data/Projects/MOU/Mammaprint/Test_set_mamaprint",
+    "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs",
+]
+MASK_DEST = "/mnt/data/Projects/MOU/Mammaprint/tissue_masks"
 LEVEL = 5
 
 
-# def process_slide(row: dict[str, Any]) -> dict[str, Any]:
-#     with OpenSlide(row["item"]) as slide:
-#         mpp_x, mpp_y = slide_resolution(slide, LEVEL)
-
-#     slide = Image.new_from_file(row["item"], level=LEVEL)
-#     mask = tissue_mask(slide, mpp=mean((mpp_x, mpp_y)))
-#     write_big_tiff(
-#         mask,
-#         path=Path(MASK_DEST, Path(row["item"]).with_suffix(".tiff").name),
-#         mpp_x=mpp_x,
-#         mpp_y=mpp_y,
-#     )
-#     return row
-
-
-@ray.remote
-def process_slide(path) -> None:
-    with OpenSlide(path) as slide:
+def process_slide(row: dict[str, Any]) -> dict[str, Any]:
+    with OpenSlide(row["item"]) as slide:
         mpp_x, mpp_y = slide_resolution(slide, LEVEL)
 
-    slide = Image.new_from_file(path, level=LEVEL)
+    slide = Image.new_from_file(row["item"], level=LEVEL)
     mask = tissue_mask(slide, mpp=mean((mpp_x, mpp_y)))
     write_big_tiff(
         mask,
-        path=Path(MASK_DEST, Path(path).with_suffix(".tiff").name),
+        path=Path(MASK_DEST, Path(row["item"]).with_suffix(".tiff").name),
         mpp_x=mpp_x,
         mpp_y=mpp_y,
     )
+    return row
 
 
 def main() -> None:
-    slides = Path(SLIDES_PATH).rglob("*.mrxs")
     Path(MASK_DEST).mkdir(exist_ok=True)
-    process_items(list(slides), process_slide)
+    slides = [
+        path for slide_path in SLIDES_PATHS for path in Path(slide_path).rglob("*.mrxs")
+    ]
+    data.from_items(slides).map(process_slide).materialize()
 
 
 if __name__ == "__main__":
