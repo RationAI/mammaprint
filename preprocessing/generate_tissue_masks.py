de5d578390b787d0ef72aa95a1e179faa7cfdd90
Matěj Pekár <525077@mail.muni.cz>
2025-03-08 18:15:07 +0100

    fix: tissue generation

--- preprocessing/generate_tissue_masks.py
+++ preprocessing/generate_tissue_masks.py
@@ -2,54 +2,33 @@ from pathlib import Path
 from statistics import mean
 from typing import Any
 
-import pyvips
-from histopath.ray.datasource import VipsTiffDatasink
 from openslide import OpenSlide
 from pyvips import Image
-from rationai.masks import slide_resolution, tissue_mask
+from rationai.masks import slide_resolution, tissue_mask, write_big_tiff
 from ray import data
-from ray.data.datasource import FilenameProvider
 
 SLIDES_PATH = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs"
 MASK_DEST = "/mnt/data/Projects/MOU/Mammaprint/Another_WSIs_tissue_masks"
 LEVEL = 5
 
 
-class Filename(FilenameProvider):
-    def get_filename_for_row(
-        self, row: dict, task_index: int, block_index: int, row_index: int
-    ) -> str:
-        return Path(row["item"]).with_suffix(".tiff").name
-
-
-def process_slide(row: dict[str, Any]) -> dict[str, Any]:
+def process_slide(row: dict[str, Any]) -> None:
     with OpenSlide(row["item"]) as slide:
         mpp_x, mpp_y = slide_resolution(slide, LEVEL)
 
-    slide = Image.new_from_file(row["item"], page=LEVEL)
-    row["tissue_mask"] = tissue_mask(slide, mpp=mean((mpp_x, mpp_y))).numpy()
-    row["options"] = {"xres": mpp_x, "yres": mpp_y}
-    return row
+    slide = Image.new_from_file(row["item"], level=LEVEL)
+    mask = tissue_mask(slide, mpp=mean((mpp_x, mpp_y)))
+    write_big_tiff(
+        mask,
+        path=Path(MASK_DEST, Path(row["item"]).with_suffix(".tiff").name),
+        mpp_x=mpp_x,
+        mpp_y=mpp_y,
+    )
 
 
 def main() -> None:
     slides = Path(SLIDES_PATH).rglob("*.mrxs")
-    data.from_items(list(slides)).map(process_slide).write_datasink(
-        VipsTiffDatasink(
-            MASK_DEST,
-            data_column="tissue_mask",
-            options_column="options",
-            filename_provider=Filename(),
-            default_options={
-                "bigtiff": True,
-                "compression": pyvips.enums.ForeignTiffCompression.DEFLATE,
-                "tile": True,
-                "tile_width": 512,
-                "tile_height": 512,
-                "pyramid": True,
-            },
-        )
-    )
+    data.from_items(list(slides)).map(process_slide).materialize()
 
 
 if __name__ == "__main__":
