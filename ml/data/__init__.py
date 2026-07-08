70649cbb64ced6389ff68944957bf8e59ce1f674
Michal Kišš <550439@mail.muni.cz>
2026-07-08 15:58:18 +0200

    feat: data

--- ml/data/__init__.py
+++ ml/data/__init__.py
@@ -1,1 +1,11 @@
 """Data utilities for MammaPrint."""
+
+from ml.data.datamodule import DataModule, mil_collate
+# from ml.data.datasets.pyramid import PyramidSlideDataset
+
+
+__all__ = [
+    "DataModule",
+    # "PyramidSlideDataset",
+    "mil_collate",
+]
