281e9ba2b252775fdd48910fb58a85759e9857a9
Michal Kišš <550439@mail.muni.cz>
2026-08-01 21:50:46 +0200

    feat: spatial

--- ml/models/aggregators/__init__.py
+++ ml/models/aggregators/__init__.py
@@ -5,6 +5,7 @@ from ml.models.aggregators.attention import AttentionMIL
 from ml.models.aggregators.base import Aggregator
 from ml.models.aggregators.max import MaxPool
 from ml.models.aggregators.mean import MeanPool
+from ml.models.aggregators.spatial_transformer import SpatialTransformerMIL
 from ml.models.aggregators.transformer import TransformerMIL
 
 
@@ -17,6 +18,7 @@ __all__ = [
     "AttentionMIL",
     "MaxPool",
     "MeanPool",
+    "SpatialTransformerMIL",
     "TransformerMIL",
     # ── Multilevel ──
     # "MultiScaleMIL",
